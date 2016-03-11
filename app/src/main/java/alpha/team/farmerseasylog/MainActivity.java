1b39820604637297c8679728162735945dfe914a
jac_robertson <jac.robertson96@gmail.com>
2016-03-11 13:21:32 +0000

    Adding buttons to main menu

--- app/src/main/java/alpha/team/farmerseasylog/MainActivity.java
+++ app/src/main/java/alpha/team/farmerseasylog/MainActivity.java
@@ -1,5 +1,7 @@
 package alpha.team.farmerseasylog;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button yourButton = (Button) findViewById(R.id.button);
+
+        yourButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), calendar.class));
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
