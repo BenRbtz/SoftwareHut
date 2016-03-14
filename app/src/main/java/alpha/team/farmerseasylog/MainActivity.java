1ee7b7c3f0072dd8df27ce7702f260966841d363
jac_robertson <jac.robertson96@gmail.com>
2016-03-14 12:54:41 +0000

    buttons update

--- app/src/main/java/alpha/team/farmerseasylog/MainActivity.java
+++ app/src/main/java/alpha/team/farmerseasylog/MainActivity.java
@@ -5,12 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity{
 
@@ -19,7 +19,7 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        Button yourButton = (Button) findViewById(R.id.button);
+        ImageButton yourButton = (ImageButton) findViewById(R.id.calendarbutton);
 
         yourButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -42,6 +42,10 @@ public class MainActivity extends Activity{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void calendarButtonOnClick(View v){
+        startActivity(new Intent(getApplicationContext(), calendar.class));
     }
 
     @Override
