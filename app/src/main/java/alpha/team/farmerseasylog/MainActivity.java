968e666f92dfe02571d75f79889c9c3d2c005a98
BenRbtz <eeu47d@bangor.ac.uk>
2016-04-19 16:21:22 +0100

    ToDoList

---
+++ app/src/main/java/alpha/team/farmerseasylog/MainActivity.java
@@ -0,0 +1,90 @@
+package alpha.team.farmerseasylog;
+
+import android.app.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    Event[] birthdays;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        birthdays = new Event[3];
+
+        new AsyncTask<Void,Void,Boolean>() {
+
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                new MongoPortal();
+
+                return true;
+            }
+
+
+
+
+        }.execute();
+
+
+
+}
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void calendarButtonOnClick(View v) {
+        startActivity(new Intent(getApplicationContext(), calendar.class));
+    }
+    public void toDoListButtonOnClick(View v) {
+        startActivity(new Intent(getApplicationContext(), toDoList.class));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/alpha/team/farmerseasylog/calendar.java
@@ -0,0 +1,44 @@
+package alpha.team.farmerseasylog;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+
+    public void calendarOnClick(View v){
+        CalendarView calendarView = (CalendarView) v;
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), "" + dayOfMonth, 0).show();
+
+            }
+        });
+
+
+    }
+}
+
+
+
+
